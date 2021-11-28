c0487b9981d67c286bd6ea13158ec5b5a65cd016
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-11-28 23:45:50 +0600

    update

---
+++ php/basic/04.php
@@ -0,0 +1,12 @@
+<?php
+$textOne = "Bangladesh";
+$textTwo = 70 ;
+$textThree = 3.1416 ;
+$textFour = NULL ;
+$textFive = array('Rifat','Abrar','Jowad');
+
+var_dump($textOne);
+var_dump($textTwo);
+var_dump($textThree);
+var_dump($textFour);
+var_dump($textFive);
