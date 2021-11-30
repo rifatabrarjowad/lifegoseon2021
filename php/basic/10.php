53607b2b9d709fc32f6b06ff33070294fd4fd32d
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-11-30 23:57:09 +0600

    adding file

--- php/basic/10.php
+++ php/basic/10.php
@@ -11,3 +11,17 @@ for ($i=0; $i < 6 ; $i++) {
     echo "Loving you alos  a losing game for you ", $i ;
     echo "\n";
 }
+// do while loop
+$x = 1;
+
+do {
+  echo "The number is: $x \n";
+  $x++;
+} while ($x <= 5);
+
+// foreach loop
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value) {
+  echo "$value \n";
+}
