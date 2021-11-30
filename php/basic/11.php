e667753910da4a14d5589699cd5f62ae468e9336
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-01 01:48:52 +0600

    adding file

---
+++ php/basic/11.php
@@ -0,0 +1,24 @@
+<?php
+function  mayName($a){
+    echo "My name is $a";
+    echo "\n";
+}
+mayName('Rifat');
+mayName('Rifat2');
+mayName('Rifat3');
+mayName('Rifat4');
+mayName('Rifat5');
+mayName('Rifat6');
+mayName('Rifat7');
+mayName('Rifat8');
+mayName('Rifat9');
+
+
+
+function hAge($h,$age)
+{
+    echo "my height $h \n my age $age";
+    echo "\n";
+    var_dump($h,$age);
+}
+hAge(5.6,17);
