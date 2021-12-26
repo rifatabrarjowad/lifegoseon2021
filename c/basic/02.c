a8f2aa4b0f1dda1dabeb2c02d7a6e19bdd134eb9
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-26 11:03:53 +0600

    laravel fress install

--- c/basic/02.c
+++ c/basic/02.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+  int a,b,sum;
+    a = 5;
+    b = 5;
+    sum = a +b;
+   printf("a + b = %d", sum);
    return 0;
 }
