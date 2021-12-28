2f2983e326802f575edace410dd28c86147449fa
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-28 18:57:59 +0600

    update file

---
+++ csharp/ConsoleApp/ConsoleApp/thisClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class thisClass
+    {
+    }
+    class Test
+    {
+
+        int num;
+        Test(int num)
+        {
+            // this.num refers to the instance field
+            this.num = num;
+            Console.WriteLine("object of this: " + this);
+        }
+
+        static void Mainaa(string[] args)
+        {
+
+            Test t1 = new Test(4);
+            Console.WriteLine("object of t1: " + t1);
+            Console.ReadLine();
+        }
+    }
+}
