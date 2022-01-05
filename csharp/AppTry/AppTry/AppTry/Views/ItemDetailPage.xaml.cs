8b24a82cecc4a147560be41d908f418ed3186f23
rifatabrarjowad <rajdiprifat2004@gmail.com>
2022-01-05 22:06:01 +0600

    update

---
+++ csharp/AppTry/AppTry/AppTry/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppTry.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppTry.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ csharp/AppTry/AppTry/AppTry/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppTry.Models;
+using AppTry.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTry.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
