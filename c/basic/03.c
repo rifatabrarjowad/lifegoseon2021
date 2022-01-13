f3912b4909d380aceff4290ceae35ab123287e2d
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2022-01-13 23:06:52 +0600

    same file

--- c/basic/03.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-  char ch;
-    printf("Enter the first letter of your name: ");
-    scanf("%c", &ch);
-    printf("The first letter of your name is: %c\n", ch);
-    scanf("%c", &ch);
-   return 0;
-}
