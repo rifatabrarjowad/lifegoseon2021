a1a434102948bb4dfd56689e347a8c994f25f4b7
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-01-20 19:42:25 +0600

    FN

---
+++ csharp/FibonacciNumber/FibonacciNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FibonacciNumber
+{
+    public class Program
+    {
+        public static int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+
+        // driver code
+        public static void Main(string[] args)
+        {
+            int n = 4;
+            Console.Write(Fib(n));
+        }
+    }
+}
