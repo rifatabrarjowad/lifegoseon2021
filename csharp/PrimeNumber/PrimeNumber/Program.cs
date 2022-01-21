e601e868662b0ec01ca68d3377ad6c0e3dc1517e
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-01-21 20:00:30 +0600

    prime number

---
+++ csharp/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PrimeNumber
+{
+    public class Program
+    {
+
+        public void disply()
+        {
+
+        }
+
+        // driver code
+        public static void Main(string[] args)
+        {
+           
+            Console.Write(FindNthFib(5));
+            var mbox = 5.56;
+            var d = mbox.GetType();
+            Console.WriteLine(d);
+        }
+
+        static int FindNthFib(int prime)
+        {
+            var stNumber = prime/2;
+            var ndNumber = prime/3;
+            var rdNumber = prime/5;
+            if (prime == 0) {
+
+                Console.WriteLine("It's deffret");
+            }else if (prime == 1) {
+                
+            }
+                 return prime;
+        }
+    }
+}
