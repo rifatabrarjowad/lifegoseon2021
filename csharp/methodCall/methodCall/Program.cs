50a7ed52532192a65d61b23ec89475213e118c4c
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-01-25 19:28:03 +0600

    method try

---
+++ csharp/methodCall/methodCall/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace methodCall
+{
+    public class Program
+    {
+
+    
+        static int addNumber()
+        {
+            var  a = 5;
+            var b = 5;
+            var  result = a + b;
+            return result;
+        }
+        public void display()
+        {
+           
+            Console.WriteLine(addNumber());
+        }
+
+        public static void Main(string[] args)
+        {
+            Program a = new Program();
+            a.display();
+        }
+
+       
+    }
+}
