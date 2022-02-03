5dd3a763ff31668ff7026251721c24cc25cbd0d8
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-03 17:53:27 +0600

    task complete

---
+++ csharp/ArrayMaxNumber/ArrayMaxNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ArrayMaxNumber
+{
+    public class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            int[]  arr = { 7, 2, 3,10,9,20 };
+            int max = arr[0];
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+
+                }
+               
+            }
+            for (int i = 1;i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Max Number of the array is =  " + max);
+
+            Console.WriteLine("Min Number of the array is =  " + min);
+        }
+    }
+}
---
+++ csharp/task/task/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace task
+{
+    class A
+    {
+        public int aa = 3;
+        public int bb = 2;
+        public virtual void Doing()
+        {
+
+            Console.WriteLine(aa + bb);
+        }
+    }
+
+    // derived class of Animal 
+    class B : A
+    {
+        public override void Doing()
+        {
+
+            Console.WriteLine(aa * bb);
+        }
+
+    }
+    class C : B
+    {
+        public override void Doing()
+        {
+            Console.WriteLine(aa - bb);
+        }
+    }
+    public class Programe
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            A a = new A();
+            a.Doing();
+            B b = new B();
+            b.Doing();
+            C c = new C();
+            c.Doing();
+        }
+
+    }
+}
+
+
+
