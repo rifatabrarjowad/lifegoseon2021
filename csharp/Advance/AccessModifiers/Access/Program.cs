58e7a46b2348d3e32d0f3e5ad8488a721dacd3c5
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-05 18:33:45 +0600

    update file

---
+++ csharp/Advance/AccessModifiers/Access/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Access
+{
+    class Programe
+    {
+       
+        static void Main(string[] args)
+        {
+            Programe a = new Programe();
+            
+            System.Console.WriteLine("Hello World! from access");
+        }
+    }
+}
---
+++ csharp/Advance/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AccessModifiers
+{
+    public class Programe
+    {
+        public void demo()
+        {
+
+        }
+        public static void  demoStatic()
+        {
+
+        }
+        protected void demoProtected()
+        {
+
+        }
+        protected static void demoProtectedStatic()
+        {
+
+        }
+        internal void demoInternal()
+        {
+
+        }
+        internal protected void demoInternalProtected()
+        {
+
+        }
+        private void demoPrivate()
+        {
+
+        }
+        private static void demoPrivateStatic()
+        {
+           
+        }
+        private protected void demoPrivateProtected()
+        {
+
+        }
+        
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+
