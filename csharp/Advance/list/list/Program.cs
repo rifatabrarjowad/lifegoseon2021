875ce013143195c9a590002f1ae7000b755b2ec0
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-17 12:35:10 +0600

    list

--- csharp/Advance/list/list/Program.cs
+++ csharp/Advance/list/list/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace list
+{
+    class Programe
+    {
+        
+        public static void Main(string[] args)
+        {
+            int[] age = new int[5] {1,2,3,4,5};
+            foreach (int i in age)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finish");
+            var name = new List<int>() { 2,3,4,9,8};
+            var indexnumber = name.IndexOf(4);
+            Console.WriteLine("Index Number finder : " + indexnumber);
+            name.Remove(2); //removeNumber
+            name.Add(5);
+           
+            foreach (var i in name)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finish");
+        }
+    }
+}
