2995ec203949f6cf54d74b9ed01a0933a824b991
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-02-27 19:15:10 +0600

    lumen api start

---
+++ php/laravel/06/blog/resources/views/wellcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    <p>@yield('header')</p>
+    <h1></h1>
+    <p>@yield('content')</p>
+
+    <p>@yield('footer')</p>
+</body>
