389ff989c4963bbd5373be7570a122b9217e3d62
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-03-01 19:20:52 +0600

    controller

---
+++ php/laravel/07/blog/app/Http/Controllers/HomeControllers.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function  robi()
+    {
+        return;
+    }
+
+    //
+}
