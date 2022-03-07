71ee195ab04701c6683723d3baaa2ac51003e69c
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-03-07 13:36:47 +0600

    Clion

---
+++ cpp/01/cmake-build-debug/CMakeFiles/01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "01.exe"
+  "01.exe.manifest"
+  "01.pdb"
+  "CMakeFiles/01.dir/main.cpp.obj"
+  "lib01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
