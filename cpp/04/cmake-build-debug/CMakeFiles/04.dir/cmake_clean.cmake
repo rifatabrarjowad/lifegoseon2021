1ecc24b3921e22c570f45be7ac7796c2019e0ca7
rifatabrajowad <rajdiprifat2004@gmail.com>
2022-03-20 14:06:25 +0600

    update

---
+++ cpp/04/cmake-build-debug/CMakeFiles/04.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "04.exe"
+  "04.exe.manifest"
+  "04.pdb"
+  "CMakeFiles/04.dir/main.cpp.obj"
+  "lib04.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/04/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    cout << "Student cgpa : " << endl;
+    getch();
+}
+// Output:
