92f6142cb825bc1f2c786ad629b6b273b6189746
rifatabrajowad <rajdiprifat2004@gmail.com>
2024-05-21 16:00:51 -0500

    nuxt js

---
+++ nuxtjs/nuxtApp/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- Your content goes here -->
+        <h1>about</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore architecto soluta, assumenda sit possimus
+            blanditiis voluptate autem eligendi laudantium velit ea explicabo delectus vitae odit qui. Libero perspiciatis
+            repudiandae reiciendis asperiores deserunt sit impedit nisi repellat perferendis debitis amet, corporis
+            molestiae voluptatibus, ut accusamus suscipit quis laboriosam. Eligendi, saepe nemo!</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Your styles go here */
+</style>
